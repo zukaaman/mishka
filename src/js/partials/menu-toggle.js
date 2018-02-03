b8c226581951c502634ab93e01d448ae20c57cc4
zukaaman <zukaaman@gmail.com>
2018-02-03 22:36:00 +0400

    делаю хедер

--- src/js/partials/menu-toggle.js
+++ src/js/partials/menu-toggle.js
@@ -1,14 +1,14 @@
 var navToggle = document.querySelector(".main-nav__toggle");
 
-navToggle.addEventListener("click", function () {
-  if (navToggle.classList.contains("main-nav__toggle--closed")) {
-    navToggle.classList.remove("main-nav__toggle--closed");
-    navToggle.classList.add("main-nav__toggle--open");
-  } else {
-    navToggle.classList.add("main-nav__toggle--closed");
-    navToggle.classList.remove("main-nav__toggle--open");
-  }
-});
+// navToggle.addEventListener("click", function () {
+//   if (navToggle.classList.contains("main-nav__toggle--closed")) {
+//     navToggle.classList.remove("main-nav__toggle--closed");
+//     navToggle.classList.add("main-nav__toggle--open");
+//   } else {
+//     navToggle.classList.add("main-nav__toggle--closed");
+//     navToggle.classList.remove("main-nav__toggle--open");
+//   }
+// });
 
 // var menuClosed = document.querySelector(".main-nav__toggle--open");
 
